fffabf0298e0be50ea559bd244cb551c9729a8b7
williamfloyd62 <william.floyd@mechanizedcyber.com>
2021-01-20 19:36:28 -0800

    working notifications

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { Button, ThemeProvider } from 'react-native-elements';
+
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -9,25 +9,63 @@ import Navigation from './navigation';
 
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
+import { Text, View, Button, Platform } from 'react-native';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
-  const registerForPushNotifications = async () => { 
-    try {
-       const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-       if (!permission.granted) return;
-       const token = await Notifications.getExpoPushTokenAsync();
-    console.log(token);
-    } catch (error) {
-      console.log('Error getting a token', error);
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
     }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
   }
 
   useEffect(() => {
-    registerForPushNotifications();
+    registerForPushNotificationsAsync().then((token) =>{ 
+      setExpoPushToken(token);
+      sendPushNotification(token);
+    });
   }, [])
+
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
 
   if (!isLoadingComplete) {
     return null;
@@ -39,5 +77,25 @@ export default function App() {
       </SafeAreaProvider>
       
     );
+  }
+
+  async function sendPushNotification(expoPushToken: any) {
+    const message = {
+      to: expoPushToken,
+      sound: 'default',
+      title: 'Original Title',
+      body: 'And here is the body!',
+      data: { data: 'goes here' },
+    };
+  
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
   }
 }
